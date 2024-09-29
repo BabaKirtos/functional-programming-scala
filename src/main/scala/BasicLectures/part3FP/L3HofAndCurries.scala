4353992af4ebcc5059b4ad7e009016036f959793
Abhishek Roy <abhiroy1993@gmail.com>
2024-09-30 00:19:24 +0530

    tailrec nTimes

--- src/main/scala/BasicLectures/part3FP/L3HofAndCurries.scala
+++ src/main/scala/BasicLectures/part3FP/L3HofAndCurries.scala
@@ -5,36 +5,47 @@ import scala.annotation.tailrec
 object L3HofAndCurries extends App {
 
   // below is an example of HOF (Higher order function)
-//  val superFunction: (Int, (String, (Int => Boolean) => Int) => (Int => Int)) = ???
+  // val superFunction: (Int, (String, (Int => Boolean) => Int) => (Int => Int)) = ???
 
   // map, flatMap, filter in MyList are examples of HOF
 
   // a function that applies a function n times over a value x
   // nTimes(f, n, x)
   // nTimes(f, 3, x) => f(f(f(x))) => if f(x) = 2 * x, n = 2 and x = 2 => 8 , this is a HOF
-  def nTimes(f: Int => Int, n: Int): Int => Int = {
-    if (n <= 0) (x: Int) => x
-    else (x: Int) => nTimes(f, n - 1)(f(x))
+  def nTimes[A](f: A => A, n: Int): A => A = {
+    if (n <= 0) (x: A) => x
+    else (x: A) => nTimes(f, n - 1)(f(x))
   }
 
-  println(nTimes(_ * 2, 4)(2))
+  println(nTimes[Int](_ * 2, 4)(2))
   // for n = 4, (x: Int) => nTimes(f, 4 - 1)(f(x))
   // for n = 3, (x: Int) => nTimes(f, 3 - 1)(f(f(x)))
   // for n = 2, (x: Int) => nTimes(f, 2 - 1)(f(f(f(x)))
   // for n = 1, (x: Int) => nTimes(f, 1 - 1)(f(f(f(f(x))))
   // for n = 0, (x: Int) => f(f(f(f(x)))
+  println(nTimes[String](_ + " TailRec ", 3)("A"))
 
   @tailrec
-  def nTimesAns(f: Int => Int, n: Int, x: Int): Int = {
-    if (n <= 0) x
-    else nTimesAns(f, n - 1, f(x))
+  def nTimesTailRec[A](f: A => A, n: Int, acc: A => A = identity[A]): A => A = {
+    if (n <= 0) acc
+    else nTimesTailRec(f, n - 1, (x: A) => f(acc(x)))
   }
-  println(nTimesAns(_ * 2, 5, 2))
 
-  println(List(1,2,3).flatMap(List(_)))
+  println(nTimesTailRec[Int](_ * 2, 4)(2))
+  println(nTimesTailRec[String](_ + " TailRec ", 3)("A"))
+
+  @tailrec
+  def nTimesValue(f: Int => Int, n: Int, x: Int): Int = {
+    if (n <= 0) x
+    else nTimesValue(f, n - 1, f(x))
+  }
+
+  println(nTimesValue(_ * 2, 5, 2))
+
+  println(List(1, 2, 3).flatMap(List(_)))
 
   val plusOne = (x: Int) => x + 1
-  println(nTimesAns(plusOne, 10, 2))
+  println(nTimesValue(plusOne, 10, 2))
 
   // curried functions
   val superAdder: Int => Int => Int = (x: Int) => (y: Int) => x + y
@@ -44,12 +55,13 @@ object L3HofAndCurries extends App {
 
   // Kunal's question
   def functionAdder(f: Int => Int)(x: Int)(y: Int): Int = f(x) + f(y)
-//  val functionAdder: (Int => Int) => Int => Int => Int = (f: Int => Int) => (x: Int) => (y: Int) => f(x) + f(y)
+  //  val functionAdder: (Int => Int) => Int => Int => Int = (f: Int => Int) => (x: Int) => (y: Int) => f(x) + f(y)
   println(functionAdder(x => x * x * x)(2)(3) + " <---")
 
   // currying
   // functions with multiple parameters list
   def doubleToString(c: String)(x: Double): String = c.format(x)
+
   // standard format will format double number passed to it
   val standardFormat: Double => String = doubleToString("%4.2f")
   val preciseFormat: Double => String = doubleToString("%10.8f")
@@ -60,24 +72,28 @@ object L3HofAndCurries extends App {
   def compose(f: Int => Int, g: Int => Int): Int => Int = {
     (x: Int) => f(g(x))
   }
+
   val addMultiply = compose(_ * 2, _ + 2)
   println(addMultiply(2))
 
   def antiCompose(f: Int => Int, g: Int => Int): Int => Int = {
     (x: Int) => g(f(x))
   }
+
   val multiplyAdd = antiCompose(_ * 2, _ + 2)
   println(multiplyAdd(2))
 
   def toCurry[A](f: (A, A) => A): A => A => A = {
     (x: A) => (y: A) => f(x, y)
   }
+
   println(toCurry[Int](_ * _)(4)(5))
   println(toCurry[String](_ + " " + _)("Hello")("World"))
 
   def fromCurry[A](f: A => A => A): (A, A) => A = {
     (x: A, y: A) => f(x)(y)
   }
+
   println(fromCurry[Int]((x: Int) => (y: Int) => x * y)(7, 8))
   println(fromCurry[String]((x: String) => (y: String) => x + " " + y)("Hello", "World"))
 
@@ -102,5 +118,4 @@ object L3HofAndCurries extends App {
        andThen(g,f) => x => g(f(x)) - done
 
    */
-
 }

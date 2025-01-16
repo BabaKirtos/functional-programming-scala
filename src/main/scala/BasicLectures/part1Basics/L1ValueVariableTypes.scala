49501b9a102f35004ad307cd1d08939cde2df8c8
Abhishek Roy <abhiroy1993@gmail.com>
2025-01-16 16:37:52 +0530

    Updates to L1ValueVariableTypes

--- src/main/scala/BasicLectures/part1Basics/L1ValueVariableTypes.scala
+++ src/main/scala/BasicLectures/part1Basics/L1ValueVariableTypes.scala
@@ -2,42 +2,45 @@ package BasicLectures.part1Basics
 
 object L1ValueVariableTypes extends App {
 
-  // Below is a perfectly good example of functions as first class citizens and
+  // Below is a perfectly good example of functions as first-class citizens and
   // how to use higher order functions to create a new list
   val addFunction = (x: Int, y: Int) => x + y // with syntactic sugar
   val multiplyFunction: (Int, Int) => Int = (x, y) => x * y // without syntactic sugar
-  val fucntionList: List[(Int, Int) => Int] = List(addFunction, multiplyFunction)
+  val functionList: List[(Int, Int) => Int] = List(addFunction, multiplyFunction)
   val inputList = List((1, 2), (3, 4), (5, 6), (7, 8))
   val partialFunction: PartialFunction[(Int, Int), List[Int]] = {
-    case (a, b) => fucntionList.map(_(a, b))
+    case (a, b) => functionList.map(_(a, b))
   }
-  val ouputMapList = inputList.map {
+  val outputMapList = inputList.map {
     partialFunction
   }
-  val ouputFlatMapList = inputList.flatMap {
+  val outputFlatMapList = inputList.flatMap {
     partialFunction
   }
-  println(fucntionList) // You get a List with 2 elements which are pointers to a lambda function (addNew, multiplyNew)
-  println(ouputMapList) // Each element of inputList will return a List
-  println(ouputFlatMapList) // We flatten the inner elements to get one outer List
+  // You get a List with 2 elements which are pointers to a lambda function (addNew, multiplyNew)
+  println(functionList)
+  // Each element of inputList will return a List
+  println(outputMapList)
+  // We flatten the inner elements to get one outer List
+  println(outputFlatMapList)
 
   val y = (a: Int) => a * 2 // Inferred type of Int => Int
   println(y)
-  println(y(2)) // think in terms of pointer, like pointer to a function
+  println(y(2)) // think in terms of pointer, like a pointer to a function
 
   val x: Int = 42
   println(x)
 
   // VALs ARE IMMUTABLE
-  // vals are like to const in C, but they are not be used in different parts of the code
-  // they are to be used in intermediate computations which would then be used in bigger computations
+  // vals are like to const in C, but they are not used in different parts of the code
+  // they are to be used in intermediate computations which would then be used in bigger computations. 
   // x = 55 will throw a compiler error
   // Compiler can infer types
   // The type of variable is written after colon
   val aString: String = "Hello"
   println(aString)
   println(aString.map(_.toUpper))
-  println(aString.flatMap(_ + " "))
+  println(aString.flatMap(_.toString + " "))
   println(aString.filter(_ == 'H'))
   aString.foreach(println(_))
 
@@ -46,7 +49,7 @@ object L1ValueVariableTypes extends App {
     a <- aString
   } println(a.toUpper)
 
-  //semicolon can be used at end, but not necessary
+  //semicolon can be used at the end, but not necessary
   val aBool: Boolean = true
   println(aBool)
 
@@ -71,8 +74,8 @@ object L1ValueVariableTypes extends App {
   val aDouble: Double = 3.14
   println(aFloat)
 
-  // variables are important as they give us side effects
-  // The side effects lets us see what our program is doing
+  // variables are important as they give us side effects, 
+  // The side effects let us see what our program is doing
   // functional programming involves working with vals than vars
   // The vars are not preferred as they are mutable and introduce side effects
   var aVariable: Int = 5 // can be reassigned
